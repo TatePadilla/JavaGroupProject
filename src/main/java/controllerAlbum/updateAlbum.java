58f76fefb25235f79128dea2237ed0757056e3ec
chris tutje <chris.tutje@gmail.com>
2023-10-11 12:17:13 -0500

    modified .jsp's and servlets for Album
    I deleted the existing jsp's because the index.html redirected to "addAlbums"(plural), and the file names were "addAlbum"(singular). If I renamed the files, it caused git issues, so I deleted and created new files.

--- src/main/java/controllerAlbum/updateAlbum.java
+++ src/main/java/controllerAlbum/updateAlbum.java
@@ -5,10 +5,45 @@
  */
 package controllerAlbum;
 
-/**
- * @author Tate
- *
- */
-public class updateAlbum {
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+import javax.persistence.*;
+import java.io.IOException;
+import model.Album;
 
+@WebServlet("/updateAlbum")
+public class updateAlbum extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String albumIdStr = request.getParameter("id");
+        String newAlbumName = request.getParameter("albumName");
+        String newBandName = request.getParameter("bandName");
+
+        try {
+            int albumId = Integer.parseInt(albumIdStr);
+
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("JavaGroupProject");
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+
+            Album album = em.find(Album.class, albumId);
+            if (album != null) {
+                album.setAlbumName(newAlbumName);
+                album.setBandName(newBandName);
+
+                em.getTransaction().commit();
+                em.close();
+
+                response.sendRedirect("updateAlbumSuccess.jsp");
+                return;
+            } else {
+                em.getTransaction().rollback();
+                em.close();
+            }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("showAlbum.jsp");
+    }
 }
